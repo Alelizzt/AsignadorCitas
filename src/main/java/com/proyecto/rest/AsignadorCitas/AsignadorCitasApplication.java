d040fd43b9a076d1d31ecfd44b35954e529b2dc6
Alejandro <alejandrolistc@hotmail.com>
2017-05-31 18:14:23 -0500

    Spring Boot Configutation

---
+++ src/main/java/com/proyecto/rest/AsignadorCitas/AsignadorCitasApplication.java
@@ -0,0 +1,12 @@
+package com.proyecto.rest.AsignadorCitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsignadorCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsignadorCitasApplication.class, args);
+	}
+}
