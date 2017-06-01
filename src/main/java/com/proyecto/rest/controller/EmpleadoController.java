0e731fa1ec89ae1900f7b79f2964d90c52081fd3
Alejandro <alejandrolistc@hotmail.com>
2017-06-01 15:57:25 -0500

    try to fix embedded container

--- src/main/java/com/proyecto/rest/controller/EmpleadoController.java
+++ src/main/java/com/proyecto/rest/controller/EmpleadoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.proyecto.rest.model.Cita;
 import com.proyecto.rest.model.Empleado;
-
+//@SpringBootApplication
 @RestController
  @RequestMapping("/api")
 public class EmpleadoController {
