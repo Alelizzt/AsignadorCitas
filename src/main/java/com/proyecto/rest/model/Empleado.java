e27f02319dc41f9879a2ecf23a1f4a96a1432d64
Alejandro <alejandrolistc@hotmail.com>
2017-05-31 19:11:47 -0500

    add Model

---
+++ src/main/java/com/proyecto/rest/model/Empleado.java
@@ -0,0 +1,65 @@
+package com.proyecto.rest.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Empleado {
+	private long id;
+	private String nombre;
+	private String apellidos;
+	private Date fechaNacimiento;
+	private List<Cita> citas;
+	
+	public Empleado(){}
+
+	public Empleado(long id, String nombre, String apellidos, Date fechaNacimiento, List<Cita> citas) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fechaNacimiento = fechaNacimiento;
+		this.citas = citas;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public List<Cita> getCitas() {
+		return citas;
+	}
+
+	public void setCitas(List<Cita> citas) {
+		this.citas = citas;
+	}
+	
+	
+}
